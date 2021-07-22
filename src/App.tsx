e9bab382c253a22ec517a67a60c025211e9303fb
liuxiaojian <oppor11520@gmail.com>
2021-07-22 16:17:04 +0800

    feat: scss 添加 scssMegic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import styles from './app.scss'
 
 function App() {
-    return <div>这是什么嘛</div>
+    return <div className={styles.app}>这是什么嘛</div>
 }
 
 export default App
